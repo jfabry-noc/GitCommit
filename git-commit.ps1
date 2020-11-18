jfabry-noc/GitCommit

commit 7d7b73058544b3e4261fb0b9d53b68d3a6d4a1db

Changed the phrasing of the commit message from commits to commit(s) so that it doesn't look quite as gross by being grammatically incorrect when only a single commit is included.

git-commit.ps1
@@ -218,7 +218,7 @@ if($commitMessageList.Count -gt 0) {
         # Commit to the repo so that Netlify can update via CI.
         WriteLog -Message "Making a new commit for $($commitMessageList.Count) commits." -Type info
         git add ./html/.
-        git commit -m "Added $($commitMessageList.Count) new commits."
+        git commit -m "Added $($commitMessageList.Count) new commit(s)."
         git push origin main
     } else {
         WriteLog -Message "Couldn't find the HTML file! Verify your repository isn't borked! Quitting..." -Type error

commit 0b9e1f7ef74f42edc083066844a192bffa3fd4d6

Fixed a bug I introduced in 7d7b730 where I changed the commit message to be grammatically correct. This broke my filter for those messages from posting to the site and creating an endless loop of posts. The filter should now work again for the new commit message, and the errant postings have been cleaned up.

git-commit.ps1
@@ -158,7 +158,7 @@ foreach($singleRepo in $repoList) {
     # Loop through the commits.
     foreach($singleCommit in $currentCommits) {
         # Check if these are legitimate commits to this repo. If not, just continue.
-        if($singleRepo.name -eq "GitCommit" -and $singleCommit.commit.message -like "Added * new commits.") {
+        if($singleRepo.name -eq "GitCommit" -and $singleCommit.commit.message -like "Added * new commit(s).") {
             continue
         }
 

html/index.html
@@ -34,54 +34,6 @@
       <p class="lead">All of the blunderous commit messages from <a href="https://github.com/jfabry-noc">John</a>.</p>
       <h2>2020 Commits</h2>
 			<!-- Watermark -->
-	<h3>GitCommit</h3>
-	<p>Added 1 new commit(s).</p>
-	<p class="date">11/18/2020 16:00:11</p>
-
-	<h3>GitCommit</h3>
-	<p>Added 1 new commit(s).</p>
-	<p class="date">11/18/2020 12:00:13</p>
-
-	<h3>GitCommit</h3>
-	<p>Added 1 new commit(s).</p>
-	<p class="date">11/18/2020 08:00:11</p>
-
-	<h3>GitCommit</h3>
-	<p>Added 1 new commit(s).</p>
-	<p class="date">11/18/2020 04:00:11</p>
-
-	<h3>GitCommit</h3>
-	<p>Added 1 new commit(s).</p>
-	<p class="date">11/18/2020 00:00:12</p>
-
-	<h3>GitCommit</h3>
-	<p>Added 1 new commit(s).</p>
-	<p class="date">11/17/2020 20:00:12</p>
-
-	<h3>GitCommit</h3>
-	<p>Added 1 new commit(s).</p>
-	<p class="date">11/17/2020 16:00:12</p>
-
-	<h3>GitCommit</h3>
-	<p>Added 1 new commit(s).</p>
-	<p class="date">11/17/2020 12:00:12</p>
-
-	<h3>GitCommit</h3>
-	<p>Added 1 new commit(s).</p>
-	<p class="date">11/17/2020 08:00:12</p>
-
-	<h3>GitCommit</h3>
-	<p>Added 1 new commit(s).</p>
-	<p class="date">11/17/2020 04:00:11</p>
-
-	<h3>GitCommit</h3>
-	<p>Added 1 new commit(s).</p>
-	<p class="date">11/17/2020 00:00:12</p>
-
-	<h3>GitCommit</h3>
-	<p>Added 2 new commit(s).</p>
-	<p class="date">11/16/2020 20:00:12</p>
-
 	<h3>CPPushAPI</h3>
 	<p>Moved the writing of the JSON file to a function and added error checking to it.</p>
 	<p class="date">11/16/2020 18:41:56</p>